3384c71e0b0c5a7607e9a2523c4191bfc270f80c
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-10-31 11:32:25 -0300

    clase tamagotchi

---
+++ tamagotchi.py
@@ -0,0 +1,9 @@
+class Tamagotchi:
+    def __init__(self, nombre, color, salud=100, felicidad=100, energia=100):
+        self.nombre = nombre
+        self.color = color
+        self.salud = salud
+        self.felicidad = felicidad
+        self.energia = energia
+
+    
