011035df2f9f6c3f356bc02c99e62c0393d7ce05
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-10-31 12:00:23 -0300

    funciona

--- tamagotchi.py
+++ tamagotchi.py
@@ -6,38 +6,45 @@ class Tamagotchi:
         self.felicidad = felicidad
         self.energia = energia
 
+
     def jugar(self):
         self.felicidad += 10
         self.salud -= 5
+
 
     def comer(self):
         self.felicidad += 5
         self.salud += 10
 
+
     def curar(self):
         self.salud += 20
         self.felicidad -= 5
+
+
 
 class Persona:
     def __init__(self, nombre, apellido, tamagotchi):
         self.nombre = nombre
         self.apellido = apellido
-        self.tamagotchi
+        self.tamagotchi = tamagotchi
+
 
     def jugar_con_tamagotchi(self):
         self.tamagotchi.jugar()
 
+
     def darle_comida(self):
         self.tamagotchi.comer()
+
 
     def curarlo(self):
         self.tamagotchi.curar()
 
-mi_tamagotchi = Tamagotchi("Pipo", "azul")
+mi_tamagotchi = Tamagotchi('Pipo', 'azul')
 
-persona = Persona("Maria", "González", mi_tamagotchi)
+persona = Persona('Maria', 'González', mi_tamagotchi)
 
 persona.darle_comida()
 persona.curarlo()
 persona.jugar_con_tamagotchi()
-
